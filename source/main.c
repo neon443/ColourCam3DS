631bac1e3f03aeade7bde8d8e8cfdcfa6790b983
neon443 <69979447+neon443@users.noreply.github.com>
2025-08-11 02:27:08 -0400

    add clearscreen and wip func

--- source/main.c
+++ source/main.c
@@ -8,6 +8,20 @@
 #define screenSize width * height * 2
 #define bufSize screenSize * 2
 
+inline void clearScreen(void) {
+	u8 *frame = gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL)
+	memset(frame, 0, 320 * 240 * 3)
+}
+
+void cleanup() {
+
+}
+
+void writePicToFBuff(void *fb, void *img, u16 x, u16 y, u16 width, u16 height) {
+	u8 *fb_8 = (u8*) img
+	u16 *img_16 = (u16*) img
+}
+
 int main(int argc, char* argv[])
 {
 	gfxInitDefault();
